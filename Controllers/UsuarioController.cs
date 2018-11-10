446ca3f1610fe872d14eebd9ba2790e30cf4b240
Karoline Suzuki <42789906+suzukikarol@users.noreply.github.com>
2018-11-10 04:06:19 -0200

    Add files via upload
    Primeira parte

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using SistemaEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace estoqueApi.Controllers
+{
+    public class UsuarioController : Controller
+    {
+
+        // GET: Produto
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(DapperORM.ReturnList<Usuario>("VerTodosUsuarios"));
+        }
+
+        [HttpPost]
+        public ActionResult AdicionarOuEditarUsuario(Usuario usuario)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@Id",usuario.Id);
+            param.Add("@Nome", usuario.Nome);
+            param.Add("@Cpf", usuario.Cpf);
+            param.Add("@Login", usuario.Login);
+            param.Add("@Senha", usuario.Senha);
+            DapperORM.ExecuteWithoutReturn("AdicionarOuEditarUsuario", param);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeletarUsuario(int Id)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@Id", Id);
+            DapperORM.ExecuteWithoutReturn("DeletarUsuario", param);
+            return RedirectToAction("Index");
+
+        }
+
+
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaEstoque.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+    }
+}
